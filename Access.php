64aa0d47b910949c411ed757867463dc73e35493
GODSKADI <aidrean.gf@gmail.com>
2018-12-04 20:07:10 +0100

    sessions + username y logout

--- Access.php
+++ Access.php
@@ -1,9 +1,15 @@
 <?php
+	session_start();
 
 	$Usuario = $_POST["InputUser"];
 	$Contrasenya = $_POST["InputPassword"];
 
-	$BBDD = new PDO('mysql:host=127.0.0.1;dbname=BD_Scrum','Dencina','P@ssw0rd');
+	if (!isset($_SESSION['InputUser'])) {
+			$_SESSION['InputUser'] = $Usuario;
+		}
+
+
+	$BBDD = new PDO('mysql:host=127.0.0.1;dbname=BD_Scrum','skadi','P@ssw0rd');
 
 	$Query = $BBDD->prepare('SELECT Nombre_Usuario, Password_Usuario FROM Usuarios WHERE Nombre_Usuario=:InputUser AND Password_Usuario=SHA2(:InputPassword,512);');
 	$Query -> bindValue(':InputUser',$Usuario);
@@ -12,7 +18,7 @@
 	$Result = $Query->rowCount();
 
 	if($Result == 1){
-		echo "CORRECTO";
+		header("Location: UserPanel.php");
 	} else {
 		echo "MAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAL";
 	}
